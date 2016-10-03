03dc1d5b9993b2d1c75f0e768b5c9dc6f34529fd
benj4183 <benj4183@CV-PC-X-16>
2016-10-03 11:28:12 +0200

    Created classes

---
+++ Services/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccess;
+using Entities;
+using Networking;
+
+namespace Services
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
