a4eb367c6b61352b8ad9fe264dd1a156f2e1eec6
benj4183 <benj4183@edu.campusvejle.dk>
2016-10-11 09:47:31 +0200

    logger er kommet længere.

---
+++ DataAccess/Logger.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entities;
+using System.IO;
+
+namespace DataAccess
+{
+    public static class Logger
+    {
+
+        private static readonly string AppPath;
+
+
+        static Logger()
+        {
+            AppPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "test.txt");
+        }
+
+        public static void Log(string message)
+        {
+            System.IO.StreamWriter file = new System.IO.StreamWriter(AppPath, true);
+
+            try
+            {                
+                file.WriteLine(message);
+            }
+            catch(IOException ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                file.Close();
+            }           
+        }
+        
+        
+    }
+}
